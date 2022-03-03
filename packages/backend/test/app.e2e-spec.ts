51d6621dceb6ad522c0101948e80284446f6cd91
Jackson Chadfield <jackson.chadfield@jacksonc.dev>
2022-03-03 14:05:27 +1300

    test: remove base e2e test

--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,12 +12,5 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
   });
 });
