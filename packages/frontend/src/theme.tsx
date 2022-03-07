87eef10eee2df20e70a120a5f20defe12e740240
GlockTian <petertianzhong@gmail.com>
2022-03-07 15:00:21 +1300

    Set teal as primary color

--- packages/frontend/src/theme.tsx
+++ packages/frontend/src/theme.tsx
@@ -4,12 +4,9 @@ export const mainTheme = createTheme({
   type: 'light',
   theme: {
     colors: {
-      primaryLight: '$green200',
-      primary: '$green400',
-      primaryDark: '$green600',
-      gradient:
-        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-      link: '#5E1DAD',
+      primaryLight: '#99f6e4', //teal200
+      primary: '#2dd4bf', //teal400
+      primaryDark: '#0d9488', //teal600
     },
     space: {},
     fonts: {},
