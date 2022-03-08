7354eec2a92bdf7599626df1b595fccdbc4b268a
Kelvin Shen <68672128+wshe874@users.noreply.github.com>
2022-03-08 21:26:13 +1300

    adding support for Firebase authentication - allow login using Google… (#50)
    * adding support for Firebase authentication - allow login using Google - implement custom hook useAuth for rerendering project when authentication status changes
    
    * Put bearer on API request
    
    * fix some formating issues
    
    Co-authored-by: Fraser McCallum <fraser@themccallums.net.nz>

--- packages/frontend/src/pages/public/SignInPage.tsx
+++ packages/frontend/src/pages/public/SignInPage.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React from 'react';
+import { auth } from '../../services/firebase';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+import firebase from 'firebase/compat/app';
+import { useAuth } from '../../hooks/useAuth';
 
 interface SignInPageProps {}
 
 const SignInPage: React.FC<SignInPageProps> = () => {
-  return <div></div>;
+  const { setSignedIn, setUser } = useAuth();
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        setSignedIn(true);
+        const R = require('ramda');
+        const userParam = R.pickAll(['displayName', 'email'], auth.currentUser);
+        setUser(userParam);
+        return false;
+      },
+    },
+  };
+
+  return (
+    <div>
+      <p>Signin</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    </div>
+  );
 };
 
 export default SignInPage;
--- packages/frontend/src/pages/routes/AuthenticatedRoutes.tsx
+++ packages/frontend/src/pages/routes/AuthenticatedRoutes.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { Routes } from "react-router";
+import React from 'react';
+import { Routes } from 'react-router';
+import { Navigate, Route } from 'react-router-dom';
+import HomePage from '../public/HomePage';
 
 interface AuthenticatedRoutesProps {}
 
 const AuthenticatedRoutes: React.FC<AuthenticatedRoutesProps> = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="home" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/home" replace />} />
+    </Routes>
+  );
 };
 
 export default AuthenticatedRoutes;
