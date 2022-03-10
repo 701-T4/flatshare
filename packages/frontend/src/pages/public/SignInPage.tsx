c53f01e1d3f33131c11398dec2b970c2c58e5980
GlockTian <petertianzhong@gmail.com>
2022-03-10 21:23:04 +1300

    remove unused import

--- packages/frontend/src/pages/public/SignInPage.tsx
+++ packages/frontend/src/pages/public/SignInPage.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Checkbox, Input, Row, Text } from '@nextui-org/react';
+import React from 'react';
+import { Text } from '@nextui-org/react';
 import { auth } from '../../services/firebase';
 import { StyledFirebaseAuth } from 'react-firebaseui';
 import firebase from 'firebase/compat/app';
