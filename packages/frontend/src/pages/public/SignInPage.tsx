8181f86220e1c418d4759007c0cf085582dfb415
GlockTian <petertianzhong@gmail.com>
2022-03-10 20:41:11 +1300

    remove email and password

--- packages/frontend/src/pages/public/SignInPage.tsx
+++ packages/frontend/src/pages/public/SignInPage.tsx
@@ -16,46 +16,19 @@ const SignInPage: React.FC<SignInPageProps> = () => {
     callbacks: {
       signInSuccessWithAuthResult: () => {
         setSignedIn(true);
-        const R = require('ramda');
-        const userParam = R.pickAll(['displayName', 'email'], auth.currentUser);
-        setUser(userParam);
+        setUser(auth.currentUser);
         return false;
       },
     },
   };
 
-  const [email, setEmail] = useState<string | undefined>(undefined);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
-
   return (
-    <div className="h-screen overflow-y-auto flex justify-center items-center  bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
-      <div className="flex flex-col items-center bg-white rounded">
-        <Text h1 size={50}>
+    <div className="h-screen flex justify-center items-center  bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
+      <div className="flex flex-col items-center ">
+        <Text h1 size={48} color="white" weight="bold">
           Log in
         </Text>
-        <div className="px-20 flex flex-col w-full">
-          <Input
-            clearable
-            bordered
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="my-4"
-            color="primary"
-            placeholder="Email"
-          />
-          <Input.Password
-            className="my-4"
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Password"
-          />
-          <Button size="lg" className="item-center">
-            Login
-          </Button>
+        <div className="px-20 py-20 flex flex-col w-full">
           <StyledFirebaseAuth
             className="w-full"
             uiConfig={uiConfig}
