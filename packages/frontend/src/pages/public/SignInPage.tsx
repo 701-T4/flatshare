49e9c79f52fb5781253323e54b7bedc84d0492da
GlockTian <petertianzhong@gmail.com>
2022-03-11 12:37:15 +1300

    add a border for it to stand out

--- packages/frontend/src/pages/public/SignInPage.tsx
+++ packages/frontend/src/pages/public/SignInPage.tsx
@@ -23,22 +23,24 @@ const SignInPage: React.FC<SignInPageProps> = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col md:flex-row justify-center items-center  bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
-      <div className="w-full lg:w-1/2 flex flex-col items-center ">
-        <Text h1 size={48} color="white" weight="bold">
-          Log in
-        </Text>
-        {/* container for all user login input*/}
-        <div className="px-20 pt-20 flex flex-col w-full">
-          <StyledFirebaseAuth
-            className="w-full"
-            uiConfig={uiConfig}
-            firebaseAuth={auth}
-          />
+    <div className="h-screen flex flex-row justify-center items-center  bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
+      <div className="flex w-full lg:w-1/2 item-center justify-center">
+        <div className="flex flex-col items-center border-4 rounded-[12px] drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
+          <Text h1 size={48} className="pt-10" color="white" weight="bold">
+            Log in
+          </Text>
+          {/* container for all user login input*/}
+          <div className="px-10 pt-20 pb-10 flex flex-col">
+            <StyledFirebaseAuth
+              className="w-full"
+              uiConfig={uiConfig}
+              firebaseAuth={auth}
+            />
+          </div>
         </div>
       </div>
       {/* right side image on large screen only*/}
-      <div className="w-1/3 bg-center bg-no-repeat bg-contain hidden lg:flex lg:h-96 bg-house" />
+      <div className="w-1/3 -mt-12 bg-center bg-no-repeat bg-contain hidden lg:flex lg:h-96 bg-house" />
     </div>
   );
 };
