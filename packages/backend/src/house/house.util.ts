a878332821baa40d6298b70f9b05d7d08391c31a
John Jia <chenhao0401@hotmail.com>
2022-03-11 12:56:26 +1300

    Change power representation

--- packages/backend/src/house/house.util.ts
+++ packages/backend/src/house/house.util.ts
@@ -5,7 +5,7 @@ import { randomInt } from 'crypto';
 @Injectable()
 export class HouseUtil {
   generateString(length) {
-    const max_safe = 281474976710655; //Max int that can be safely reached by randomInt (2^48 - 1)
+    const max_safe = 2 ** 48 - 1; //Max int that can be safely reached by randomInt
     return randomInt(max_safe).toString(36).substr(1, length).toUpperCase();
   }
 }
