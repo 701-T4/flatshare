3e61f7258321c8bddb164fe878c2cd33932fe477
FriedhelmWS <352063465@qq.com>
2022-03-11 16:44:50 +1300

    Sign up with email input fields

--- packages/frontend/src/pages/public/SignInPage.tsx
+++ packages/frontend/src/pages/public/SignInPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text } from '@nextui-org/react';
+import { Text, Input, Spacer, Button } from '@nextui-org/react';
 import { auth } from '../../services/firebase';
 import { StyledFirebaseAuth } from 'react-firebaseui';
 import firebase from 'firebase/compat/app';
 import { useAuth } from '../../hooks/useAuth';
+import { MailIcon, LockOpenIcon } from '@heroicons/react/outline';
 import '../../styles/firebaseui-styling.global.css';
 
 interface SignInPageProps {}
@@ -24,24 +25,60 @@ const SignInPage: React.FC<SignInPageProps> = () => {
   };
 
   return (
-    <div className="h-screen flex flex-row justify-center items-center  bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
-      <div className="flex w-full lg:w-1/2 item-center justify-center">
+    <div className="flex flex-row items-center justify-center h-screen bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
+      <div className="flex justify-center w-full lg:w-1/2 item-center">
         <div className="flex flex-col items-center bg-white rounded-xl drop-shadow-xl">
           <Text h1 size={48} className="pt-10" color="black" weight="bold">
             Log in
           </Text>
           {/* container for all user login input*/}
-          <div className="px-14 pt-20 pb-10 flex flex-col">
+          <div className="flex flex-col pb-10 px-14">
             <StyledFirebaseAuth
               className="w-full"
               uiConfig={uiConfig}
               firebaseAuth={auth}
             />
+            {/* the text with line before and after */}
+            <div className="flex items-center py-5 ">
+              <div className="flex-grow border-t border-black"></div>
+              <div className="px-2 pb-1">
+                <Text weight="semibold">Or sign up with e-mail</Text>
+              </div>
+              <div className="flex-grow border-t border-black"></div>
+            </div>
+            {/* input fields group starts */}
+            <Input
+              contentLeft={<MailIcon style={{ height: '100%' }} />}
+              placeholder="Your email"
+            />
+            <Spacer y={0.5} />
+            <Input
+              contentLeft={<LockOpenIcon style={{ height: '100%' }} />}
+              placeholder="Your password"
+            />
+            <Spacer y={0.5} />
+            <Input
+              contentLeft={<LockOpenIcon style={{ height: '100%' }} />}
+              placeholder="Confirm password"
+            />
+            <Button
+              className="my-5"
+              style={{
+                backgroundColor: '#2596A4',
+              }}
+            >
+              Sign Up
+            </Button>
+            <div className="flex justify-center">
+              <Button size="xs" light>
+                <Text size={12}>Already have an account?</Text>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
       {/* right side image on large screen only*/}
-      <div className="w-1/3 -mt-12 bg-center bg-no-repeat bg-contain hidden lg:flex lg:h-96 bg-house" />
+      <div className="hidden w-1/3 -mt-12 bg-center bg-no-repeat bg-contain lg:flex lg:h-96 bg-house" />
     </div>
   );
 };
