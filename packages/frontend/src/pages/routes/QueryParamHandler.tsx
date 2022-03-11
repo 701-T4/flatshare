58ad98af2f0dfae39cf4c01de26955afee9b1146
Fraser McCallum <fraser@themccallums.net.nz>
2022-03-11 22:48:59 +1300

    Fix sharing link code bugs

--- packages/frontend/src/pages/routes/QueryParamHandler.tsx
+++ packages/frontend/src/pages/routes/QueryParamHandler.tsx
@@ -1,67 +1,56 @@
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useAlert } from '../../components/common/util/CornerAlert';
-import {
-  getUrl,
-  useApi,
-  useApiMutation,
-  useMutatingApi,
-} from '../../hooks/useApi';
+import { getUrl, useApi, useApiMutation } from '../../hooks/useApi';
 import { useAuth } from '../../hooks/useAuth';
 import { ExclamationCircleIcon } from '@heroicons/react/solid';
+import { auth } from '../../services/firebase';
 
 interface QueryParamHandlerProps {}
 
 const QueryParamHandler: React.FC<QueryParamHandlerProps> = ({ children }) => {
   useJoinCode();
-
   return <>{children}</>;
 };
 
 // ?join
 const useJoinCode = () => {
   const { createAlert } = useAlert();
-  const { authLoaded, signedIn } = useAuth();
-  const { data, loading } = useApi('/api/v1/user', { method: 'get' });
+  const { signedIn } = useAuth();
+  const { data, loading, error } = useApi('/api/v1/user', { method: 'get' });
 
   // check if the url has a join code OR localstorage has a join code
   const [searchParams, setSearchParams] = useSearchParams();
-  const inviteCode = searchParams.get('join') ?? '';
+  const urlJoinCode = searchParams.get('join') ?? '';
 
   const joinHouseMutation = useApiMutation('/api/v1/house', { method: 'put' });
 
-  const joinHouse = (houseCode: string) => {
-    joinHouseMutation({
-      body: {
-        houseCode,
-      },
-    });
-  };
+  useEffect(() => {
+    if (urlJoinCode) {
+      localStorage.setItem('joinCode', urlJoinCode);
+    }
+  }, [urlJoinCode]);
 
   useEffect(() => {
-    if (!authLoaded) {
+    const joinHouse = (houseCode: string) => {
+      console.log(`Try join ${houseCode}`);
+      joinHouseMutation({
+        body: { houseCode },
+      });
+    };
+
+    const clearParam = () => {
+      console.log('Clearing Params');
+      localStorage.removeItem('joinCode');
+      searchParams.delete('joinCode');
+      setSearchParams(searchParams);
+    };
+
+    const joinCode = localStorage.getItem('joinCode');
+
+    if (!signedIn || loading || error || !joinCode) {
       return;
     }
-    if (!signedIn) {
-      // save to localstorage stuff
-      localStorage.setItem('code', inviteCode);
-      console.log('Stored');
-      return;
-    }
-
-    if (loading) {
-      return;
-    }
-
-    const joinCode = inviteCode ?? localStorage.get('join');
-
-    if (!joinCode) {
-      return;
-    }
-
-    localStorage.removeItem('join');
-    searchParams.delete('join');
-    setSearchParams(searchParams);
 
     if (data?.house) {
       createAlert(
@@ -72,28 +61,23 @@ const useJoinCode = () => {
         },
         3000,
       );
-    } else {
-      joinHouse(joinCode);
-      console.log('1.');
+      clearParam();
+      return;
     }
 
-    // if (localStorage.getItem('code')) {
-    //   localStorage.getItem('code');
-    //   console.log('Retrieved Code');
-    //   localStorage.removeItem('code');
-    //   console.log('Removed Code');
-    //   return;
-    // } else if (data?.house) {
-    //   joinHouse(joinCode);
-    //   console.log('2.');
-    //   return;
-    // }
-    return;
-
-    // check for URL code or localstorage
-
-    // join using that code
-  }, [authLoaded, signedIn, data, loading]);
+    joinHouse(joinCode);
+    clearParam();
+  }, [
+    urlJoinCode,
+    signedIn,
+    loading,
+    error,
+    data,
+    joinHouseMutation,
+    searchParams,
+    setSearchParams,
+    createAlert,
+  ]);
 };
 
 export default QueryParamHandler;
--- packages/frontend/src/pages/routes/Router.tsx
+++ packages/frontend/src/pages/routes/Router.tsx
@@ -1,83 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useAuth } from '../../hooks/useAuth';
 import AuthenticatedRoutes from './AuthenticatedRoutes';
 import UnauthenticatedRoutes from './UnauthenticatedRoutes';
-import { getAuth } from 'firebase/auth';
-import { useAuth } from '../../hooks/useAuth';
-import { useLocation, useSearchParams } from 'react-router-dom';
 
 interface RouterProps {}
 
 const Router: React.FC<RouterProps> = () => {
-  const { authLoaded, setAuthLoaded, setUser, signedIn } = useAuth();
-  // const [searchParams] = useSearchParams();
-  // const inviteCode1 = searchParams.get('code') ?? '';
+  const { signedIn } = useAuth();
 
-  // const location = useLocation();
-  // let path = location.pathname;
-  // const inviteCode = path.slice(path.indexOf('=') + 1);
-  // const joinCheck = path.slice(1, 5);
-
-  // // change to  api call
-  // const getUserHouseCode = () => {
-  //   return { data: null };
-  // };
-  // const { data: code } = getUserHouseCode();
-
-  // const joinHouse = () => {
-  //   if (localStorage.getItem('code')) {
-  //     console.log('user added to the house');
-  //   }
-  // };
-
-  useEffect(() => {
-    // returns function to stop the listener
-    const clearListener = getAuth().onAuthStateChanged((user) => {
-      setUser(user);
-      setAuthLoaded(true);
-    });
-    return () => {
-      clearListener();
-    };
-  }, [setUser, setAuthLoaded]);
-
-  // console.log({ authLoaded, signedIn });
-
-  // if (!authLoaded) {
-  //   return null;
-  // }
-
-  // if (!signedIn) {
-  //   // Route to sign-in page with stored code
-
-  //   if (joinCheck === 'join') {
-  //     localStorage.setItem('code', inviteCode);
-  //     console.log('Stored');
-  //   }
-  //   return <UnauthenticatedRoutes />;
-  // }
-
-  // if (signedIn) {
-  //   if (joinCheck === 'join') {
-  //     if (localStorage.getItem('code')) {
-  //       localStorage.getItem('code');
-  //       console.log('Retrieved Code');
-  //       if (code === null) {
-  //         joinHouse();
-  //         console.log('1.');
-  //       } else {
-  //         return <AuthenticatedRoutes />;
-  //       }
-  //       localStorage.removeItem('code');
-  //       console.log('Removed Code');
-  //       return <AuthenticatedRoutes />;
-  //     } else if (code === null) {
-  //       joinHouse();
-  //       console.log('2.');
-  //       return <AuthenticatedRoutes />;
-  //     }
-  //     return <AuthenticatedRoutes />;
-  //   }
-  // }
+  if (!signedIn) {
+    return <UnauthenticatedRoutes />;
+  }
 
   return <AuthenticatedRoutes />;
 };
