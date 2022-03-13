4719c301bc2b9a11c320bfb31756e49b7e5226e5
wqsz7xn <wqsz7xn@protonmail.com>
2022-03-14 10:52:11 +1300

    fix module exports

--- packages/backend/src/db/db.module.ts
+++ packages/backend/src/db/db.module.ts
@@ -4,13 +4,21 @@ import { UserStoreService } from './user/userStore.service';
 import { User, UserSchema } from './user/user.schema';
 import { HouseStoreService } from './house/houseStore.service';
 import { House, HouseSchema } from './house/house.schema';
+import { NoteStoreService } from './note/noteStore.service';
+import { Note, NoteSchema } from './note/note.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: House.name, schema: HouseSchema }]),
+    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
   ],
-  providers: [UserStoreService, HouseStoreService],
-  exports: [MongooseModule, UserStoreService, HouseStoreService],
+  providers: [UserStoreService, HouseStoreService, NoteStoreService],
+  exports: [
+    MongooseModule,
+    UserStoreService,
+    HouseStoreService,
+    NoteStoreService,
+  ],
 })
 export class DbModule {}
