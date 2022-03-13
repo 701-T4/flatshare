5140c1d86c40dd3287e33064b846387fc67f7de9
Oscar <lioscar79@gmail.com>
2022-03-13 22:24:25 +1300

    Remove husky from backend package

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -20,6 +20,5 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
