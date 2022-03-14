ce7bb1ebe8ff84f4ac67c7ab248132c184b15f70
FriedhelmWS <352063465@qq.com>
2022-03-14 19:28:19 +1300

    Basic bills list implementation

--- packages/frontend/src/components/dashboard/upcoming-tasks/UpcomingTask.tsx
+++ packages/frontend/src/components/dashboard/upcoming-tasks/UpcomingTask.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import cx from 'classnames';
 import { CheckCircleIcon } from '@heroicons/react/outline';
+import { Switch } from '@nextui-org/react';
 
 const Variation = {
   red: 'bg-gradient-to-r from-red-400 to-red-600',
   amber: 'bg-gradient-to-r from-amber-400 to-amber-600',
   teal: 'bg-gradient-to-r from-teal-400 to-teal-600',
   purple: 'bg-gradient-to-r from-purple-400 to-purple-600',
+  gray: 'bg-gradient-to-r from-gray-600 to-gray-400',
 };
 
 interface UpcomingTaskProps {
@@ -36,15 +38,20 @@ const UpcomingTask: React.FC<UpcomingTaskProps> & {
             'text-left rounded-t-xl px-4 py-1 text-white font-semibold text-lg',
           )}
         >
-          {type}
+          <div className="flex flex-row justify-between">
+            {type}
+            {type === 'Bill' && !completed && (
+              <Switch className="self-end mb-1 mr-5" size="sm"></Switch>
+            )}
+          </div>
         </div>
-        <div className="rounded-b-xl px-4 lg:px-8 py-4 flex flex-col gap-y-1 bg-gray-800 h-full">
-          <div className="flex justify-between items-center">
+        <div className="flex flex-col h-full px-4 py-4 bg-gray-800 rounded-b-xl lg:px-8 gap-y-1">
+          <div className="flex items-center justify-between">
             <div>
-              <div className="font-bold text-base md:text-xl text-teal-500">
+              <div className="text-base font-bold text-teal-500 md:text-xl">
                 {title}
               </div>
-              <div className="font-medium text-xs md:text-sm text-white">
+              <div className="text-xs font-medium text-white md:text-sm">
                 {dueString}
               </div>
             </div>
@@ -55,7 +62,11 @@ const UpcomingTask: React.FC<UpcomingTaskProps> & {
               )}
               onClick={clickWrapper}
             >
-              {completed ? <CheckCircleIcon className="w-6" /> : <>Complete</>}
+              {completed ? (
+                <CheckCircleIcon className="w-6" />
+              ) : (
+                <>{type === 'Bill' ? 'Detail' : 'Complete'}</>
+              )}
             </button>
           </div>
         </div>
--- packages/frontend/src/pages/private/BillSplittingPage.tsx
+++ packages/frontend/src/pages/private/BillSplittingPage.tsx
@@ -1,9 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Page from '../../components/common/layout/Page';
+import UpcomingTask from '../../components/dashboard/upcoming-tasks/UpcomingTask';
+import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
+import { useAuth } from '../../hooks/useAuth';
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router';
 
 interface BillSplittingPageProps {}
 
 const BillSplittingPage: React.FC<BillSplittingPageProps> = () => {
-  return <>Hello</>;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const adhocUpcomingBillIds = [
+    {
+      name: 'Countdown',
+      description: 'I go for buy some eat',
+      owner: 'firebaseID',
+      due: 1647215067300,
+      users: [
+        {
+          id: 'cZWbFn471ihGFtlMBVKo3DrH1D43',
+          amount: 30, //$
+          paid: false,
+          proof: 'blob id', //optional
+        },
+        {
+          id: 'firebaseId',
+          amount: 20, //$
+          paid: false,
+          proof: 'blob id', //optional
+        },
+      ],
+    },
+  ];
+  const [adhocPastBillIds, setAdhocPastBillIds] = useState([2, 4]);
+
+  return (
+    <Page>
+      <div className="flex flex-col gap-4">
+        <UnderlinedText colorClasses="bg-gray-800">
+          <div className="text-lg font-semibold">Upcoming Bills</div>
+        </UnderlinedText>
+        {adhocUpcomingBillIds.map((bill, index) => {
+          var amount;
+          for (let index in bill.users)
+            if (bill.users[index].id === user?.uid)
+              amount = bill.users[index].amount;
+
+          return (
+            <UpcomingTask
+              title={`${bill.name} - $${amount}`}
+              dueString={bill.due.toString()}
+              twColor={UpcomingTask.Variation.red}
+              type="Bill"
+              onCompleteClick={() => navigate('/bills/1')}
+            />
+          );
+        })}
+        <UnderlinedText className="pt-10" colorClasses="bg-gray-800">
+          <div className="text-lg font-semibold">Past Bills</div>
+        </UnderlinedText>
+        <div className="flex flex-col gap-4 mt-4 md:grid md:grid-cols-2">
+          {adhocPastBillIds.map((bill, index) => (
+            <UpcomingTask
+              title="Take out the Rubbish"
+              dueString="Done"
+              twColor={UpcomingTask.Variation.gray}
+              type="Bill"
+              completed
+            />
+          ))}
+        </div>
+
+        <Button
+          className="w-16 mt-3 bg-gray-500"
+          onClick={() => setAdhocPastBillIds([...adhocPastBillIds, 0, 0, 0, 0])}
+        >
+          Load More
+        </Button>
+      </div>
+    </Page>
+  );
 };
 
 export default BillSplittingPage;
