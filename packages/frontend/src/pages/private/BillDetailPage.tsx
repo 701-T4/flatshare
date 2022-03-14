ed6044aa670fa5df0b028ee44970fa572ae7b9d7
GlockTian <petertianzhong@gmail.com>
2022-03-14 19:36:46 +1300

    Styling

--- packages/frontend/src/pages/private/BillDetailPage.tsx
+++ packages/frontend/src/pages/private/BillDetailPage.tsx
@@ -11,18 +11,19 @@ const BillDetailPage: React.FC<BillDetailPageProps> = () => {
     {
       id: 1,
       name: "GinToki's home rental",
-      description: 'GinToki! pay your home rental now!',
+      description:
+        'GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!GinToki! pay your home rental now!',
       owner: 'firebaseID',
       due: 1647215067300,
       users: [
         {
-          id: 'firebaseId',
+          id: 'firebaseId fassdas',
           amount: 30, //$
           paid: false,
           proof: 'blob id', //optional
         },
         {
-          id: 'firebaseId1',
+          id: 'Kvi306xYuzSDQm3nJKQ42LLMKSC3',
           amount: 30, //$
           paid: false,
           proof: 'blob id', //optional
@@ -30,7 +31,7 @@ const BillDetailPage: React.FC<BillDetailPageProps> = () => {
         {
           id: 'firebaseId2',
           amount: 30, //$
-          paid: false,
+          paid: true,
           proof: 'blob id', //optional
         },
         {
@@ -47,14 +48,19 @@ const BillDetailPage: React.FC<BillDetailPageProps> = () => {
   const auth = getAuth();
   const userId = auth.currentUser?.uid;
   console.log([id, ' in Bill Detail Page']);
+  console.log([userId, ' is the current User']);
 
   // replace with backend call
   const bill = bills.find((b) => b.id === parseInt(id!));
 
+  const isOwner = userId === bill?.owner;
+
   const onUpload = async (userId: string) => {};
 
+  const toggleComplete = async () => {};
+
   return (
-    <div className="pt-10 px-20">
+    <div className="pt-10 px-10 md:px-20">
       <div className="shadow-lg rounded-b-xl">
         <div className="flex flex-col h-full">
           <div
@@ -68,53 +74,50 @@ const BillDetailPage: React.FC<BillDetailPageProps> = () => {
               className={
                 'text-white flex justify-center items-center transition-all bg-teal-500 hover:bg-teal-400 rounded-full px-4 py-2 font-medium h-fit'
               }
-              onClick={() => onUpload('')}
+              onClick={() => toggleComplete()}
             >
               Complete
             </button>
           </div>
-          <div className="rounded-b-xl px-4 lg:px-8 py-4 flex flex-col gap-y-1 bg-gray-800 h-full">
-            <div className="flex justify-between items-center">
-              <div>
-                <DetailRow
-                  title="Description"
-                  value={bill?.description!}
-                ></DetailRow>
+          <div className="rounded-b-xl px-4 lg:px-8 py-4 flex flex-col items-center gap-y-1 bg-gray-800 h-full">
+            <div className="flex flex-col lg:flex-row justify-between items-start">
+              <div className="flex flex-col lg:w-1/2">
+                <DetailRow title="Description" value={bill?.description!} />
                 <Spacer y={2} />
-                <DetailRow
-                  title="Due"
-                  value={bill?.due?.toString()!}
-                ></DetailRow>
+                <DetailRow title="Due" value={bill?.due?.toString()!} />
                 <Spacer y={2} />
-                <div className="font-bold text-base mt-2 md:text-3xl text-teal-500">
-                  Payment
-                </div>
-                {bill?.users.map((u) => (
-                  <div key={u.id} className="pt-8 flex flex-row items-center">
-                    <div className="font-bold text-base md:text-xl text-teal-500">
-                      {u.id}
-                    </div>
-                    <Spacer x={1} />
-                    {u.paid ? (
-                      <div className="font-bold text-base md:text-xl text-white">
-                        Paid
-                      </div>
-                    ) : (
-                      <div className="font-bold text-base md:text-xl text-white">
-                        ${u.amount}
-                      </div>
-                    )}
-                    <Spacer x={2} />
-                    <button
-                      className={
-                        'text-white flex justify-center items-center transition-all bg-teal-500 hover:bg-teal-400 rounded-full px-4 py-2 font-medium h-fit'
-                      }
-                      onClick={() => onUpload(u.id)}
+              </div>
+              <div className="flex flex-col lg:w-1/2">
+                <div className="font-bold text-3xl text-teal-500">Payment</div>
+                <div className="flex flex-col">
+                  {bill?.users.map((u) => (
+                    <div
+                      key={u.id}
+                      className="flex flex-col w-full items-start py-2"
                     >
-                      Upload
-                    </button>
-                  </div>
-                ))}
+                      <div className="font-bold text-base md:text-xl text-white">
+                        {u.id.toString()}
+                      </div>
+                      <div className="flex flex-row w-full justify-between items-start">
+                        <div className="font-bold text-base md:text-2xl text-white">
+                          {u.paid ? <>Paid</> : <>${u.amount}</>}
+                        </div>
+                        {userId === u.id ? (
+                          <button
+                            className={
+                              'text-white flex justify-center items-center transition-all bg-teal-500 hover:bg-teal-400 rounded-full px-4 py-2 font-medium h-fit'
+                            }
+                            onClick={() => onUpload(u.id)}
+                          >
+                            Upload
+                          </button>
+                        ) : (
+                          <div className=""></div>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
@@ -130,39 +133,11 @@ interface DetailRowProps {
 }
 const DetailRow: React.FC<DetailRowProps> = ({ title, value }) => {
   return (
-    <div className="flex flex-row">
-      <div className="font-bold text-base md:text-xl text-teal-500">
+    <div className="flex flex-col items-start">
+      <div className="font-bold text-base md:text-2xl text-teal-500">
         {title}
       </div>
-      <Spacer x={0.5} />
       <div className="font-bold text-base md:text-xl text-white">{value}</div>
-    </div>
-  );
-};
-
-interface UserPaymentProps {
-  userName: string;
-  amount: string;
-  isPaid: boolean;
-}
-const UserPayment: React.FC<UserPaymentProps> = ({
-  userName,
-  amount,
-  isPaid,
-}) => {
-  return (
-    <div className="flex flex-row">
-      <div className="font-bold text-base md:text-xl text-teal-500">
-        {userName}
-      </div>
-      <Spacer x={0.5} />
-      {isPaid ? (
-        <div className="font-bold text-base md:text-xl text-white">
-          {amount}
-        </div>
-      ) : (
-        <div className="font-bold text-base md:text-xl text-white">Paid</div>
-      )}
     </div>
   );
 };
