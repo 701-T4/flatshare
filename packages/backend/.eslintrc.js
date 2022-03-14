7b8f080fe9bb0ec366161c2a827e1e9731962858
ljey973 <ljey973@aucklanduni.ac.nz>
2022-03-14 19:54:21 +1300

    Changed explicit-module-boundary-types to error from off.

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -19,6 +19,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
   },
 };
