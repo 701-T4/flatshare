20cf86e1d813a10bfc71e357c484cfb6a4dc5c63
ljey973 <ljey973@aucklanduni.ac.nz>
2022-03-16 02:14:10 +1300

    Solved merge conflict

--- packages/backend/src/controllers/users/users.controller.ts
+++ packages/backend/src/controllers/users/users.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
 import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiOkResponse,
   ApiOperation,
@@ -12,6 +20,10 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '../../util/user.decorator';
 import { DecodedIdToken } from 'firebase-admin/auth';
 import UserResponseDto from './dto/user-response.dto';
+import UserTasksResponseDto from './dto/user-tasks-response.dto';
+import { Task } from 'src/db/task/task.schema';
+import { TaskStoreService } from 'src/db/task/taskStore.service';
+import { Console } from 'console';
 
 @ApiTags('users')
 @Controller('/api/v1/user')
@@ -19,6 +31,7 @@ export class UsersController {
   constructor(
     private readonly userStoreService: UserStoreService,
     private readonly houseStoreService: HouseStoreService,
+    private readonly taskStoreService: TaskStoreService,
   ) {}
 
   @Post()
@@ -53,5 +66,32 @@ export class UsersController {
       firebaseId: userDoc.firebaseId,
       house: houseDoc?.code,
     };
+  }
+
+  @Get('/tasks')
+  @Auth()
+  @ApiOperation({ summary: 'get all active tasks assigned to a user' })
+  @ApiOkResponse({
+    description: 'tasks retrieved successfully',
+    type: UserTasksResponseDto,
+  })
+  @ApiBadRequestResponse({ description: 'user does not exist' })
+  async getTasksForUser(
+    @User() user: DecodedIdToken,
+  ): Promise<UserTasksResponseDto | null> {
+    const userDoc = await this.userStoreService.findOneByFirebaseId(user.uid);
+
+    if (userDoc != undefined) {
+      const tasks = await this.taskStoreService.findAll();
+      const tasksForUser = tasks.filter(
+        (task) => task.assigned === userDoc.firebaseId,
+      );
+      console.log(tasks);
+      console.log(tasksForUser);
+      return {
+        tasks: tasksForUser,
+      };
+    }
+    throw new HttpException('user is not in a house', HttpStatus.BAD_REQUEST);
   }
 }
