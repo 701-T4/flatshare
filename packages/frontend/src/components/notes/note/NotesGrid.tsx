d9d4ae479bb63a5fa336240dcf279ab25852ffd3
RahulA2000 <100628368+RahulA2000@users.noreply.github.com>
2022-03-19 18:44:41 +1300

    Updated Notes Grid to return Card

--- packages/frontend/src/components/notes/note/NotesGrid.tsx
+++ packages/frontend/src/components/notes/note/NotesGrid.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Button, Text, Grid, Card } from '@nextui-org/react';
 
-interface NotesGridProps {}
+interface NotesGridProps {
+  name?: string;
+  type?: string;
+}
 
-const NotesGrid: React.FC<NotesGridProps> = ({}) => {
-  return <div></div>;
+const NotesGrid: React.FC<NotesGridProps> = ({ name, type }) => {
+  return (
+    <div>
+      <Card>
+        <Text>{name}</Text>
+        <Text>{type}</Text>
+      </Card>
+    </div>
+  );
 };
 
 export default NotesGrid;
