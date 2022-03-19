ab651f94c82cb7b34c929bee78e7856d8e346f1c
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-03-19 23:07:38 +1300

    fix: response schema

--- packages/backend/src/controllers/tasks/dto/house-tasks-response.dto.ts
+++ packages/backend/src/controllers/tasks/dto/house-tasks-response.dto.ts
@@ -2,6 +2,8 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { TaskResponseDto } from './task-response-dto';
 
 export default class HouseTasksResponseDto {
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    type: [TaskResponseDto],
+  })
   tasks: TaskResponseDto[];
 }
