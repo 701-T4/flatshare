df09402b1c9aa117315fbb4a5e0d73248f1d432d
RahulA2000 <100628368+RahulA2000@users.noreply.github.com>
2022-03-19 17:34:09 +1300

    Notes Page Update

--- packages/frontend/src/pages/private/NotesPage.tsx
+++ packages/frontend/src/pages/private/NotesPage.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import Page from '../../components/common/layout/Page';
-import { useHouse } from '../../hooks/useHouse';
-import NoteCardController from '../../components/notes/note/noteCardController';
+import { Button, Text, Grid, Card } from '@nextui-org/react';
+import { PlusIcon } from '@heroicons/react/outline';
+import NewNoteController from '../../components/notes/note/newNoteController';
+import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
 
 interface NotesProps {}
 
 const NotesPage: React.FC<NotesProps> = () => {
-  
-  const { name } = useHouse();
-
   return (
     <Page>
-      <NoteCardController />
+      <div className="flex justify-between items-center pb-1">
+        <UnderlinedText colorClasses="from-gray-800 via-teal-700 to-teal-500 ">
+          <div className="text-lg font-large">{'Notes'}</div>
+        </UnderlinedText>
+        <Button icon={<PlusIcon />}>Create Note</Button>
+      </div>
+      <Grid.Container gap={2} justify="center">
+        <Grid xs={12}>
+          <Card color="primary">
+            <Text h4 color="white" size={30}>
+              Note Name
+            </Text>
+            <Text color="white" size={16}>
+              {' '}
+              Note Type
+            </Text>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Page>
   );
 };
