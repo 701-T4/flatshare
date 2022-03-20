aafb7a12ca4186087d769fadb711b6471bf7c195
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-03-20 18:03:41 +1300

    chore: fix task hook

--- packages/frontend/src/hooks/useTask.tsx
+++ packages/frontend/src/hooks/useTask.tsx
@@ -1,55 +1,36 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext } from 'react';
 import { useApi } from './useApi';
 
 interface TasksContext {
-  tasks?: Tasks;
-  refetchCurrentTasks: (value: Tasks) => void;
-}
-interface Tasks {
-  tasks?: {
-    name: string;
-    description: string;
-    isComplete: boolean;
-    dueDate: string;
-    interval: number;
-    assigned: string;
-    pool: string[];
-  }[];
+  tasks?: Task[];
 }
 
-const initialValue: Tasks = {
-  tasks: [
-    {
-      name: '',
-      description: '',
-      isComplete: false,
-      dueDate: new Date().toUTCString(),
-      interval: 0,
-      assigned: '',
-      pool: [],
-    },
-  ],
-};
+interface Task {
+  name: string;
+  description: string;
+  isComplete: boolean;
+  dueDate: string;
+  interval: number;
+  assigned: string;
+  pool: string[];
+}
+
+const initialValue: Task[] = [];
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 const TasksContext = createContext<TasksContext>({
-  tasks: undefined,
-  refetchCurrentTasks: () => null,
+  tasks: initialValue,
 });
 
 export const useTask = () => useContext<TasksContext>(TasksContext);
 
 export const TasksContextProvider: React.FC<{}> = ({ children }) => {
-  const [currentTasks, setCurrentTasks] = useState<Tasks>(initialValue);
-  const { data: tasks } = useApi('/api/v1/house/tasks', { method: 'get' });
-  console.log(tasks);
-  setCurrentTasks(tasks?.tasks as Tasks);
-
-  const refetchCurrentTasks = () => {
-    const { data: tasks } = useApi('/api/v1/house/tasks', { method: 'get' });
-    setCurrentTasks(tasks?.tasks as Tasks);
-  };
+  const { data: res } = useApi('/api/v1/house/tasks', { method: 'get' });
+  const tasks = res?.tasks?.sort(
+    (a, b) => Date.parse(a.dueDate) - Date.parse(b.dueDate),
+  );
 
   return (
-    <TasksContext.Provider value={{ ...tasks?.tasks, refetchCurrentTasks }}>
+    <TasksContext.Provider value={{ tasks: tasks ?? [] }}>
       {children}
     </TasksContext.Provider>
   );
--- packages/frontend/src/pages/private/TaskPage.tsx
+++ packages/frontend/src/pages/private/TaskPage.tsx
@@ -1,73 +1,19 @@
+// import { getAuth } from 'firebase/auth';
 import React from 'react';
-import UpcomingTask from '../../components/dashboard/upcoming-tasks/UpcomingTask';
 import Page from '../../components/common/layout/Page';
-import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
-import { useHouse } from '../../hooks/useHouse';
-import { useNavigate } from 'react-router';
 import CreateNewTaskButton from '../../components/task/task/CreateNewTaskButton';
 import CreateTaskModal from '../../components/task/task/CreateTaskModal';
+import { useTask } from '../../hooks/useTask';
 
 interface TaskPageProps {}
 
 const TaskPage: React.FC<TaskPageProps> = () => {
-  const setNavigate = useNavigate();
-  const { name } = useHouse();
-  const mockTaskData = [
-    {
-      type: `Incomplete assigned to ${name}`,
-      getMockTask: () =>
-        [...Array(4)].map((_, i) => (
-          <UpcomingTask
-            title="Take out the Rubbish"
-            dueString="Due Tomorrow"
-            twColor={UpcomingTask.Variation.red}
-            type="Task"
-            onCompleteClick={() => setNavigate('/tasks/1')}
-          />
-        )),
-    },
-    {
-      type: 'Other incomplete tasks',
-      getMockTask: () =>
-        [...Array(2)].map((_, i) => (
-          <UpcomingTask
-            title="Take out the Rubbish"
-            dueString="Due Tomorrow"
-            twColor={UpcomingTask.Variation.purple}
-            type="Task"
-            onCompleteClick={() => setNavigate('/tasks/1')}
-          />
-        )),
-    },
-    {
-      type: `Complete tasks assigned to ${name}`,
-      getMockTask: () =>
-        [...Array(2)].map((_, i) => (
-          <UpcomingTask
-            title="Take out the Rubbish"
-            dueString="Due Tomorrow"
-            twColor={UpcomingTask.Variation.amber}
-            type="Task"
-            completed
-            onCompleteClick={() => setNavigate('/tasks/1')}
-          />
-        )),
-    },
-    {
-      type: 'Other complete tasks',
-      getMockTask: () =>
-        [...Array(3)].map((_, i) => (
-          <UpcomingTask
-            title="Take out the Rubbish"
-            dueString="Due Tomorrow"
-            twColor={UpcomingTask.Variation.teal}
-            type="Task"
-            completed
-            onCompleteClick={() => setNavigate('/tasks/1')}
-          />
-        )),
-    },
-  ];
+  // const setNavigate = useNavigate();
+  // const auth = getAuth();
+  const { tasks } = useTask();
+
+  // console.log(auth.currentUser?.displayName);
+  console.log(tasks);
 
   const [visibleCreateModal, setVisibleCreateModal] = React.useState(false);
 
@@ -82,18 +28,7 @@ const TaskPage: React.FC<TaskPageProps> = () => {
       <div className="flex flex-row justify-end">
         <CreateNewTaskButton onClick={openModalHandler} />
       </div>
-      <div className="flex flex-col gap-8">
-        {mockTaskData.map((item) => (
-          <div>
-            <UnderlinedText colorClasses="from-gray-800 via-teal-700 to-teal-500">
-              <div className="text-lg font-semibold">{item.type}</div>
-            </UnderlinedText>
-            <div className="flex flex-col gap-4 mt-4 md:grid md:grid-cols-2">
-              {item.getMockTask()}
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-col gap-8"></div>
       <CreateTaskModal
         visible={visibleCreateModal}
         setVisible={setVisibleCreateModal}
