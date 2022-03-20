5d3595563f711b333bbde8df671e66b36877c27e
Fraser McCallum <fraser@themccallums.net.nz>
2022-03-20 19:57:21 +1300

    Fix warning

--- packages/frontend/src/pages/private/BillSplittingPage.tsx
+++ packages/frontend/src/pages/private/BillSplittingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import Page from '../../components/common/layout/Page';
 import UpcomingTask from '../../components/dashboard/upcoming-tasks/UpcomingTask';
 import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
