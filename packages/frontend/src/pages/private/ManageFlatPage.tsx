6ea5cfff7488cde580b3bd25be3fcecb70a70d9d
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-28 22:55:53 +1300

    add titles to manage flat page

--- packages/frontend/src/pages/private/ManageFlatPage.tsx
+++ packages/frontend/src/pages/private/ManageFlatPage.tsx
@@ -1,12 +1,27 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import Page from '../../components/common/layout/Page';
+import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
+import { useHouse } from '../../hooks/useHouse';
 
 interface ManageFlatPageProps {}
 
 const ManageFlatPage: React.FC<ManageFlatPageProps> = () => {
+  const house = useHouse();
+  console.log(house);
+
   return (
     <Page>
-      <div className="flex flex-col gap-4"></div>
+      <div className="flex flex-col gap-4">
+        <UnderlinedText colorClasses="bg-gray-800">
+          <div className="text-lg font-semibold">
+            {house.name || 'Your Flat'}
+          </div>
+        </UnderlinedText>
+
+        <UnderlinedText className="pt-10" colorClasses="bg-gray-800">
+          <div className="text-lg font-semibold">Past Bills</div>
+        </UnderlinedText>
+      </div>
     </Page>
   );
 };
