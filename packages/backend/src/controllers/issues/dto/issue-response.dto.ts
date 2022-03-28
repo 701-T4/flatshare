37fa371df478b046db28fc29d39b3b246f6fc629
Joel <64521958+butterplup@users.noreply.github.com>
2022-03-28 20:57:21 +1300

    Backend - Maintenance Issues CRUD (#146)
    * feat: add issue model and service
    
    * test: added tests for the services
    
    * Create Issue DTOs
    
    * feat: added controller for all endpoints
    
    Co-authored-by: Lance Delos Reyes <lan_drey19@hotmail.com>, Sherman Chin <schi314@aucklanduni.ac.nz>

---
+++ packages/backend/src/controllers/issues/dto/issue-response.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class IssueResponseDto {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  logger: string;
+
+  @ApiPropertyOptional()
+  loggedDate: number;
+
+  @ApiProperty()
+  resolved: boolean;
+}
+
+export class IssuesResponseDto {
+  @ApiProperty({
+    type: [IssueResponseDto],
+  })
+  issues: IssueResponseDto[];
+}
