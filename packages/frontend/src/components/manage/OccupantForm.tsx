17b5457694cce1d0f016e49a884b4cf334aa1755
hkim532 <68993476+hajineats@users.noreply.github.com>
2022-03-29 21:06:09 +1300

    Fix unable to consecutively type characters

--- packages/frontend/src/components/manage/OccupantForm.tsx
+++ packages/frontend/src/components/manage/OccupantForm.tsx
@@ -1,7 +1,7 @@
-import { Container, Row, Button } from '@nextui-org/react';
+import { Container, Row, Button, Col, Text, Input } from '@nextui-org/react';
 import React, { useContext, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
 import { OccupantCardProps } from './OccupantCard';
-import InputField from './OccupantInputField';
 
 import { PanelContext } from './OccupantPanel';
 
@@ -15,46 +15,100 @@ export const InputContext = React.createContext<InputContextInterface>(
 );
 
 const OccupantForm: React.FC<OccupantCardProps> = (props) => {
-  const [occupantFormData, setOccupantFormData] = useState<OccupantCardProps>({
-    ...props,
-  });
+  const [percentage, setPercentage] = useState<number>(
+    props.percentageOfRent || 0,
+  );
+  const [contact, setContact] = useState<string>(props.contact || '');
+  const [joinDate, setJoinDate] = useState<Date>(props.dateJoined as Date);
+  const [endDate, setEndDate] = useState<Date>(props.contractEndDate as Date);
+
   const { ownerView, onSaveOccupant, onDeleteOccupant } =
     useContext(PanelContext);
   return (
-    <InputContext.Provider value={{ occupantFormData, setOccupantFormData }}>
-      <Container xl style={{ padding: 0 }}>
-        <Row>
-          <InputField
-            field={'percentageOfRent'}
-            text={'Percentage of rent to pay'}
-            displayPercentage
+    <Container xl style={{ padding: 0 }}>
+      <Row>
+        <Col>
+          <Text className="mb-1" color={'secondary'} size={15}>
+            {'Percentage of rent to pay'}
+          </Text>
+          <Input
+            readOnly={!ownerView}
+            initialValue={props.percentageOfRent?.toString()}
+            aria-label={'Percentage of rent to pay'}
+            onChange={(e) => setPercentage(Number(e.target.value))}
+            color="secondary"
+            labelRight={'%'}
+            width="60%"
           />
-          <InputField field={'contact'} text={'Contact number'} />
-        </Row>
-        <Row className="mt-4">
-          <InputField field={'dateJoined'} text={'Date joined'} />
-          <InputField field={'contractEndDate'} text={'Contract ending date'} />
-        </Row>
-        <div className="flex justify-end mt-12 mr-5 ">
-          {ownerView && (
-            <>
-              <Button
-                onClick={() => onDeleteOccupant(occupantFormData.firebaseId)}
-                className="inline-block bg-btn_red"
-              >
-                Remove Occupant
-              </Button>
-              <Button
-                onClick={() => onSaveOccupant(occupantFormData)}
-                className="inline-block ml-4 bg-btn_green"
-              >
-                Save
-              </Button>
-            </>
-          )}
-        </div>
-      </Container>
-    </InputContext.Provider>
+        </Col>
+        <Col>
+          <Text className="mb-1" color={'secondary'} size={15}>
+            {'Contact number'}
+          </Text>
+          <Input
+            readOnly={!ownerView}
+            initialValue={props.contact}
+            onChange={(e) => setContact(e.target.value)}
+            aria-label={'Contact number'}
+            color="secondary"
+            width="60%"
+          />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <Text className="mb-1" color={'secondary'} size={15}>
+            {'Date joined'}
+          </Text>
+          <ReactDatePicker
+            className="appearance-none rounded-xl h-10 p-1 pl-2 text-sm text-black w-3/5"
+            selected={joinDate}
+            dateFormat="MMMM d, yyyy"
+            onChange={(date: Date) => setJoinDate(date)}
+            popperProps={{ strategy: 'fixed' }}
+            popperPlacement={'auto'}
+          />
+        </Col>
+        <Col>
+          <Text className="mb-1" color={'secondary'} size={15}>
+            {'Contract ending date'}
+          </Text>
+          <ReactDatePicker
+            className="appearance-none rounded-xl h-10 p-1 pl-2 text-sm text-black w-3/5"
+            selected={endDate}
+            dateFormat="MMMM d, yyyy"
+            onChange={(date: Date) => setEndDate(date)}
+            popperProps={{ strategy: 'fixed' }}
+            popperPlacement={'auto'}
+          />
+        </Col>
+      </Row>
+      <div className="flex justify-end mt-12 mr-5 ">
+        {ownerView && (
+          <>
+            <Button
+              onClick={() => onDeleteOccupant(props.firebaseId)}
+              className="inline-block bg-btn_red"
+            >
+              Remove Occupant
+            </Button>
+            <Button
+              onClick={() => {
+                const newOccupantData: OccupantCardProps = { ...props };
+                newOccupantData.contact = contact;
+                newOccupantData.contractEndDate = endDate;
+                newOccupantData.dateJoined = joinDate;
+                newOccupantData.percentageOfRent = percentage;
+                onSaveOccupant(newOccupantData);
+              }}
+              className="inline-block ml-4 bg-btn_green"
+            >
+              Save
+            </Button>
+          </>
+        )}
+      </div>
+    </Container>
   );
 };
 
