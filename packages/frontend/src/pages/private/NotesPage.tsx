cb38419ffaa93baf9b2323c109c1edf8fcf4763d
ckar364 <ckar364@aucklanduni.ac.nz>
2022-03-30 12:41:38 +1300

    chore: modify notes dashboard heading

--- packages/frontend/src/pages/private/NotesPage.tsx
+++ packages/frontend/src/pages/private/NotesPage.tsx
@@ -20,8 +20,8 @@ const NotesPage: React.FC<NotesProps> = () => {
       <div className="flex justify-between items-center pb-1">
         <UnderlinedText colorClasses="from-gray-800 via-teal-700 to-teal-500 ">
           <div className="text-lg font-medium">
-            Create Note
-            <span className="text-teal-500 font-semibold">{name}</span>
+            <span className="text-teal-500 font-semibold">{name}</span> Notes
+            Dashboard
           </div>
         </UnderlinedText>
         <Button
