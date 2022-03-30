d66622e754d73712e446d9efd4699bd6853a4565
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-31 02:29:15 +1300

    add flat information card to manage flat page

--- packages/frontend/src/pages/private/ManageFlatPage.tsx
+++ packages/frontend/src/pages/private/ManageFlatPage.tsx
@@ -2,12 +2,17 @@ import React, { useCallback, useMemo, useState } from 'react';
 import Page from '../../components/common/layout/Page';
 import UnderlinedText from '../../components/dashboard/GradientUnderlinedText';
 import { useHouse } from '../../hooks/useHouse';
+import { useAuth } from '../../hooks/useAuth';
+import HouseSettings from '../../components/manage/HouseSettings';
 
 interface ManageFlatPageProps {}
 
 const ManageFlatPage: React.FC<ManageFlatPageProps> = () => {
   const house = useHouse();
-  console.log(house);
+  const { user } = useAuth();
+
+  console.log('house', house);
+  console.log('user', user);
 
   return (
     <Page>
@@ -17,6 +22,15 @@ const ManageFlatPage: React.FC<ManageFlatPageProps> = () => {
             {house.name || 'Your Flat'}
           </div>
         </UnderlinedText>
+        <HouseSettings
+          ownerView={true}
+          houseName={house.name || 'Your Flat'}
+          totalRent={house.rent}
+          maxNumOccupants={house.maxOccupants}
+          address={house.address}
+          ownerContactNum={house.email}
+          onUpdateHouse={() => {}}
+        />
 
         <UnderlinedText className="pt-10" colorClasses="bg-gray-800">
           <div className="text-lg font-semibold">Past Bills</div>
