762023c050be0611e70cc2c80bb416793783fb73
butterplup <joelkahuhutch@gmail.com>
2022-03-31 17:31:54 +1300

    Backend - Maintenance Issues CRUD (#146)

---
+++ packages/backend/src/controllers/issues/issues.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { IssueController } from './issues.controller';
+import { DbModule } from '../../db/db.module';
+import { FirebaseAuthStrategy } from '../../guards/firebase.auth';
+import { IssueUtil } from './issues.util';
+
+@Module({
+  controllers: [IssueController],
+  imports: [DbModule],
+  providers: [IssueUtil, FirebaseAuthStrategy],
+})
+export class IssueModule {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+}
---
+++ packages/backend/src/db/issue/issue.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type IssueDocument = Issue & Document;
+
+@Schema()
+export class Issue {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'House' })
+  house: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  logger: Types.ObjectId;
+
+  @Prop({ type: Date, default: new Date() })
+  loggedDate: Date;
+
+  @Prop({ default: false })
+  resolved: boolean;
+}
+
+export const IssueSchema = SchemaFactory.createForClass(Issue);
+
+export class IssueModel {
+  name: string;
+  description: string;
+  house: Types.ObjectId;
+  logger: Types.ObjectId;
+  loggedDate: Date;
+  resolved: boolean;
+}
