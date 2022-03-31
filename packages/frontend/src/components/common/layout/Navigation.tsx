492aaefe3c17bc97eb5aaaa92fcbfbc72c685883
cameron <chol840@aucklanduni.ac.nz>
2022-03-31 23:19:01 +1300

    flatshare logo now returns users to the dashboard

--- packages/frontend/src/components/common/layout/Navigation.tsx
+++ packages/frontend/src/components/common/layout/Navigation.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import AppLogo from '../util/AppLogo';
 import UserDisplay from '../util/UserDisplay';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@nextui-org/react';
 
 interface NavigationProps {}
 
 const Navigation: React.FC<NavigationProps> = () => {
+  const navigate = useNavigate();
   return (
     <div className="p-4 shadow mb-8">
       <div className="flex justify-between items-center mx-10">
-        <AppLogo />
+        <Button
+          className="bg-transparent w-auto h-auto"
+          onClick={() => navigate('/dashboard')}
+        >
+          <AppLogo />
+        </Button>
         <UserDisplay />
       </div>
     </div>
