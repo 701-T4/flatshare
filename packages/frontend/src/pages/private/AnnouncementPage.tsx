cde3e4a5f9eaf6b0a0182d9ba01c8ba3edbcf5fe
rainasong <rainasong0509@gmail.com>
2022-04-01 22:31:36 +1300

    Add basic AnnouncementPage structure with hardcoded data

--- packages/frontend/src/pages/private/AnnouncementPage.tsx
+++ packages/frontend/src/pages/private/AnnouncementPage.tsx
@@ -1,12 +1,126 @@
-import React, { useState } from 'react';
+import { Button } from '@nextui-org/react';
+import React, { useEffect, useState } from 'react';
+import AnnouncementCard, {
+  AnnouncementCardProps,
+} from '../../components/announcement/AnnouncementCard';
+import AnnouncementModalForm from '../../components/announcement/AnnouncementModalForm';
 import Page from '../../components/common/layout/Page';
 
 interface AnnouncementPageProps {}
 
 const AnnouncementPage: React.FC<AnnouncementPageProps> = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [announcementList, setAnnouncementList] = useState<
+    AnnouncementCardProps[]
+  >([]);
+
+  const handleOnSave = (title: string, detail: string) => {
+    console.log('title: ', title);
+    console.log('details: ', detail);
+  };
+
+  const fakedata: AnnouncementCardProps[] = [
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+    {
+      title: 'title',
+      detail: 'detail',
+      time: 'time',
+      userName: 'usename',
+    },
+  ];
+
+  useEffect(() => {
+    setAnnouncementList(fakedata);
+  }, []);
+
   return (
     <Page>
-      <div>announcement page</div>
+      <div>
+        <Button className="rounded-lg" onClick={() => setModalVisible(true)}>
+          New Announcement
+        </Button>
+        <AnnouncementModalForm
+          visible={modalVisible}
+          setVisible={setModalVisible}
+          onSaveAnnouncement={handleOnSave}
+        />
+        <div className="flex flex-col mt-8 gap-y-3">
+          {announcementList.length > 0 ? (
+            announcementList.map((announcement) => {
+              return (
+                <AnnouncementCard
+                  title={announcement.title}
+                  detail={announcement.detail}
+                  time={announcement.time}
+                  userName={announcement.userName}
+                />
+              );
+            })
+          ) : (
+            <div>No announcements found, post one now :)</div>
+          )}
+        </div>
+      </div>
     </Page>
   );
 };
