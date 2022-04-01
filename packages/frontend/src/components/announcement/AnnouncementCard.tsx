cf176bcd8b210f0caca19b9d7fa925ad3563c12a
rainasong <rainasong0509@gmail.com>
2022-04-02 01:27:26 +1300

    Update announcement modal and card styles

--- packages/frontend/src/components/announcement/AnnouncementCard.tsx
+++ packages/frontend/src/components/announcement/AnnouncementCard.tsx
@@ -11,6 +11,7 @@ const AnnouncementCard: React.FC<AnnouncementCardProps> = (props) => {
   return (
     <>
       <Card
+        className="px-3 pt-5 pb-3"
         shadow={false}
         css={{
           backgroundColor: '#1D2530',
@@ -22,12 +23,14 @@ const AnnouncementCard: React.FC<AnnouncementCardProps> = (props) => {
           <span className="self-start text-xl font-bold text-white">
             {props.title}
           </span>
-          <span className="self-end text-m text-white">
+          <span className="self-end text-sm font-semibold text-white">
             {props.time}, {props.userName}
           </span>
         </div>
         <Spacer y={0.5} />
-        <span className="text-m text-white">{props.detail}</span>
+        <span className="text-base leading-relaxed text-justify text-white">
+          {props.detail}
+        </span>
         <Spacer y={0.5} />
       </Card>
     </>
