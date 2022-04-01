1d2acc9c5a83c23cb214e4d326c0612461af4472
Mercury Lin <62003343+lin8231@users.noreply.github.com>
2022-04-02 02:36:00 +1300

    style description of announcement

--- packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
+++ packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
@@ -10,10 +10,22 @@ const DashboardAnnouncement: React.FC<DashboardAnnouncementProps> = ({
 }) => {
   return (
     <div className="rounded-xl p-6 mb-6 bg-gray-800  w-full flex flex-row justify-between text-white space-x-6">
-      <div className="flex flex-col h-full "></div>
+      <div className="flex flex-col h-full max-w-full overflow-hidden">
+        <h3 className="text-xl font-bold tracking-normal mb-2">
+          Flat agreement rules
+        </h3>
+        <p className="text-xs mb-3">
+          {'Time'}, {'Username'}
+        </p>
+        <p className="truncate ...">
+          {
+            'Description Description Description Description Description Description Description Description Description Description DescriptionDescriptionDescriptionDescription Description '
+          }
+        </p>
+      </div>
       <Button
         aria-label="View All Announcements"
-        className="bg-teal-500"
+        className="bg-teal-500 self-center"
         onClick={onViewAll}
       >
         View All Announcements
