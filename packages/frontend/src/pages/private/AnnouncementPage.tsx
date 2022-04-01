f5c3bceb39a02968f020c2e20b7940ef948c0080
rainasong <rainasong0509@gmail.com>
2022-04-01 22:00:02 +1300

    Add new accouncement page and card on the dashboard

---
+++ packages/frontend/src/pages/private/AnnouncementPage.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Page from '../../components/common/layout/Page';
+
+interface AnnouncementPageProps {}
+
+const AnnouncementPage: React.FC<AnnouncementPageProps> = () => {
+  return (
+    <Page>
+      <div>announcement page</div>
+    </Page>
+  );
+};
+
+export default AnnouncementPage;
