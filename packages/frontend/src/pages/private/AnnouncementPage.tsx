9590f08f713f9433d3ed2a2f6c718f46d49e9361
rainasong <rainasong0509@gmail.com>
2022-04-02 01:12:34 +1300

    Add API logics to announcement page

--- packages/frontend/src/pages/private/AnnouncementPage.tsx
+++ packages/frontend/src/pages/private/AnnouncementPage.tsx
@@ -1,97 +1,50 @@
 import { Button } from '@nextui-org/react';
 import React, { useEffect, useState } from 'react';
-import AnnouncementCard, {
-  AnnouncementCardProps,
-} from '../../components/announcement/AnnouncementCard';
+import AnnouncementCard from '../../components/announcement/AnnouncementCard';
 import AnnouncementModalForm from '../../components/announcement/AnnouncementModalForm';
 import Page from '../../components/common/layout/Page';
+import { useApi, useApiMutation } from '../../hooks/useApi';
 
-interface AnnouncementPageProps {}
+interface AnnouncementResponseProp {
+  title?: string | undefined;
+  description?: string | undefined;
+  author?: string;
+  houseCode?: string;
+  dateCreated?: string;
+}
 
-const AnnouncementPage: React.FC<AnnouncementPageProps> = () => {
+const AnnouncementPage: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
   const [announcementList, setAnnouncementList] = useState<
-    AnnouncementCardProps[]
+    AnnouncementResponseProp[]
   >([]);
 
-  const handleOnSave = (title: string, detail: string) => {
-    console.log('title: ', title);
-    console.log('details: ', detail);
+  const [loading, setLoading] = useState(true);
+
+  const publishAnnouncement = useApiMutation('/api/v1/house/announcements', {
+    method: 'post',
+  });
+
+  const handleOnSave = async (title: string, description: string) => {
+    const data = {
+      title: title,
+      description: description,
+    };
+    await publishAnnouncement({ body: data });
+    window.location.reload();
   };
 
-  const fakedata: AnnouncementCardProps[] = [
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-    {
-      title: 'title',
-      detail: 'detail',
-      time: 'time',
-      userName: 'usename',
-    },
-  ];
+  const { data } = useApi('/api/v1/house/announcements', {
+    method: 'get',
+  });
 
   useEffect(() => {
-    setAnnouncementList(fakedata);
-  }, []);
+    if (data !== undefined) {
+      setAnnouncementList(data.announcements);
+      setLoading(false);
+    }
+  }, [data]);
 
   return (
     <Page>
@@ -104,22 +57,27 @@ const AnnouncementPage: React.FC<AnnouncementPageProps> = () => {
           setVisible={setModalVisible}
           onSaveAnnouncement={handleOnSave}
         />
-        <div className="flex flex-col mt-8 gap-y-3">
-          {announcementList.length > 0 ? (
-            announcementList.map((announcement) => {
-              return (
-                <AnnouncementCard
-                  title={announcement.title}
-                  detail={announcement.detail}
-                  time={announcement.time}
-                  userName={announcement.userName}
-                />
-              );
-            })
-          ) : (
-            <div>No announcements found, post one now :)</div>
-          )}
-        </div>
+        {!loading && (
+          <div className="flex flex-col mt-8 gap-y-3">
+            {announcementList.length > 0 ? (
+              announcementList.map((announcement, index) => {
+                return (
+                  <AnnouncementCard
+                    key={index}
+                    title={announcement.title!}
+                    detail={announcement.description!}
+                    time={new Date(
+                      announcement.dateCreated!,
+                    ).toLocaleDateString()}
+                    userName={announcement.author!}
+                  />
+                );
+              })
+            ) : (
+              <div>No announcements found, post one now :)</div>
+            )}
+          </div>
+        )}
       </div>
     </Page>
   );
