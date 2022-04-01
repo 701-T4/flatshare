df5f8445a823790e97fd5a6a7be61135fe7afee2
Mercury Lin <62003343+lin8231@users.noreply.github.com>
2022-04-02 02:12:28 +1300

    add view all button

--- packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
+++ packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@nextui-org/react';
 import React from 'react';
 
 interface DashboardAnnouncementProps {
@@ -10,6 +11,13 @@ const DashboardAnnouncement: React.FC<DashboardAnnouncementProps> = ({
   return (
     <div className="rounded-xl p-6 mb-6 bg-gray-800  w-full flex flex-row justify-between text-white space-x-6">
       <div className="flex flex-col h-full "></div>
+      <Button
+        aria-label="View All Announcements"
+        className="bg-teal-500"
+        onClick={onViewAll}
+      >
+        View All Announcements
+      </Button>
     </div>
   );
 };
