e87e38f178c36e309e41c150ed856852a9d4a179
rainasong <rainasong0509@gmail.com>
2022-04-03 10:04:38 +1200

    Update signin and landing page styles

--- packages/frontend/src/pages/public/LandingPage.tsx
+++ packages/frontend/src/pages/public/LandingPage.tsx
@@ -6,7 +6,7 @@ interface LandingPageProps {}
 
 const LandingPage: React.FC<LandingPageProps> = () => {
   return (
-    <div className="flex flex-col h-screen overflow-auto pb-28 gap-y-12 bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
+    <div className="flex flex-col h-screen overflow-auto gap-y-14 bg-gradient-to-b from-land_page_bg_start to-land_page_bg_end">
       <div className="flex flex-row items-center justify-between gap-4 p-8 sm:p-12">
         {/* Top bar with logo and the button */}
         <div className="flex flex-row items-center justify-start gap-2 sm:gap-3">
@@ -17,7 +17,7 @@ const LandingPage: React.FC<LandingPageProps> = () => {
         </div>
         <Link to="/auth">
           <Button
-            className="float-right px-5 sm:px-6 sm:py-5"
+            className="float-right px-5 text-base font-medium text-white sm:px-6 sm:py-5"
             bordered
             auto
             style={{
@@ -25,12 +25,12 @@ const LandingPage: React.FC<LandingPageProps> = () => {
               display: 'flex',
             }}
           >
-            <p className="text-base text-white sm:text-lg">Log In</p>
+            Log In
           </Button>
         </Link>
       </div>
 
-      <div className="grid self-center max-w-screen-xl grid-cols-1 gap-4 px-12 lg:grid-cols-2">
+      <div className="grid self-center justify-center h-full max-w-screen-xl grid-cols-1 gap-4 px-12 lg:ml-28 min-w-fit md:grid-cols-2">
         {/* left side area with texts */}
         <div className="flex flex-col items-start">
           <h1 className="text-6xl font-bold leading-normal text-white sm:leading-relaxed sm:text-7xl">
@@ -59,7 +59,7 @@ const LandingPage: React.FC<LandingPageProps> = () => {
           </div>
         </div>
         {/* right side image */}
-        <div className="hidden w-full bg-center bg-no-repeat bg-contain sm:inline h-96 bg-house" />
+        <div className="self-center hidden w-full ml-10 bg-center bg-no-repeat bg-contain lg:ml-0 lg:self-auto md:inline h-96 bg-house" />
       </div>
     </div>
   );
