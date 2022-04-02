0e2e72ed5925cd9c7be7cc7cd2532a4b9d0973cd
Sherman <90881342+schi314@users.noreply.github.com>
2022-04-02 15:23:45 +1300

    Add image upload for issue creation (#190)
    * Add image upload support during issue creation
    
    * Remove unused values

--- packages/frontend/src/components/issue/NewIssueCard.tsx
+++ packages/frontend/src/components/issue/NewIssueCard.tsx
@@ -1,6 +1,11 @@
 import { Button, Textarea } from '@nextui-org/react';
 import React, { useState } from 'react';
 import { useApiMutation } from '../../hooks/useApi';
+import { parseFileName } from '../../services/fileNameParser';
+import { getStorage, ref, uploadBytes, UploadResult } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+import { CloudUploadIcon } from '@heroicons/react/outline';
+import { useAlert } from '../../components/common/util/CornerAlert';
 
 interface NewIssueCardProps {
   refetchOptimisticIssues: (issue: any) => void;
@@ -11,6 +16,7 @@ interface Issue {
   title: string;
   detail: string;
   resolved: boolean;
+  image: File | null | undefined;
 }
 
 const NewIssueCard: React.FC<NewIssueCardProps> = ({
@@ -21,14 +27,17 @@ const NewIssueCard: React.FC<NewIssueCardProps> = ({
     title: '',
     detail: '',
     resolved: false,
+    image: undefined,
   });
 
   const createIssue = useApiMutation('/api/v1/house/issues', {
     method: 'post',
   });
 
+  const { createAlert } = useAlert();
+
   const handleDoneButton = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    let issue = {
+    const issue = {
       name: issueInfo.title,
       description: issueInfo.detail,
       resolved: issueInfo.resolved,
@@ -39,9 +48,41 @@ const NewIssueCard: React.FC<NewIssueCardProps> = ({
       body: issue,
     };
 
+    if (issueInfo.image) {
+      createAlert({
+        icon: <CloudUploadIcon />,
+        message: 'Wait while image is being uploaded!',
+        mode: 'info',
+      });
+
+      try {
+        issue.image = (await uploadImage()).ref.name;
+
+        createAlert({
+          icon: <CloudUploadIcon />,
+          message: 'Upload Success!',
+          mode: 'info',
+        });
+      } catch (err) {
+        createAlert({
+          icon: <CloudUploadIcon />,
+          message: 'Image upload failed',
+          mode: 'info',
+        });
+      }
+    }
+
     refetchOptimisticIssues(issue);
     await createIssue(issueBody);
     refetchFromApi();
+  };
+
+  const uploadImage = async (): Promise<UploadResult> => {
+    const storage = getStorage();
+    const fileName = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    const storageRef = ref(storage, fileName);
+
+    return uploadBytes(storageRef, issueInfo.image!, {});
   };
 
   return (
@@ -95,6 +136,30 @@ const NewIssueCard: React.FC<NewIssueCardProps> = ({
                   }))
                 }
               />
+            </div>
+            <div className="flex flex-col gap-y-0.5">
+              <div className="font-bold">Image</div>
+              <label
+                htmlFor="file-upload"
+                className="flex justify-center px-4 py-2 font-medium text-white transition-all bg-teal-500 rounded-full bold hover:bg-teal-400 h-fit w-fit"
+              >
+                <input
+                  id="file-upload"
+                  hidden
+                  type={'file'}
+                  onChange={(e) => {
+                    setIssueInfo((prev) => ({
+                      ...prev,
+                      image: e.currentTarget.files?.item(0),
+                    }));
+                  }}
+                ></input>
+                <span className="max-w-xs overflow-hidden text-right text-clip">
+                  {issueInfo.image
+                    ? parseFileName(issueInfo.image.name)
+                    : 'Select File'}
+                </span>
+              </label>
             </div>
           </div>
         </div>
