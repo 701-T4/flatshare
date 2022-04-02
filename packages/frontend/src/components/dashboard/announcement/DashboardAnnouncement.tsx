87d9edaa627ebda06e21f1bf9dc9a8be59d8846f
rainasong <rainasong0509@gmail.com>
2022-04-03 02:04:14 +1300

    Update dashboard announcement styling and default avatar image

--- packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
+++ packages/frontend/src/components/dashboard/announcement/DashboardAnnouncement.tsx
@@ -17,7 +17,7 @@ const DashboardAnnouncement: React.FC<DashboardAnnouncementProps> = ({
   description,
 }) => {
   return (
-    <div className="flex flex-col justify-between w-full p-6 mb-6 space-x-8 text-white bg-gray-800 sm:flex-row rounded-xl">
+    <div className="flex flex-col justify-between w-full p-6 mb-6 text-white bg-gray-800 sm:gap-x-8 sm:flex-row rounded-xl">
       <div className="flex flex-col h-full max-w-full overflow-hidden">
         <h3 className="mb-2 text-xl font-bold tracking-normal">{title}</h3>
         <p className="mb-3 text-sm font-semibold">
@@ -25,13 +25,15 @@ const DashboardAnnouncement: React.FC<DashboardAnnouncementProps> = ({
         </p>
         <p className="text-base font-normal truncate ...">{description}</p>
       </div>
-      <Button
-        aria-label="View All Announcements"
-        className="self-center mt-5 bg-teal-500 sm:mt-0"
-        onClick={onViewAll}
-      >
-        View All Announcements
-      </Button>
+      <div className="flex justify-end w-full sm:w-auto">
+        <Button
+          aria-label="View All Announcements"
+          className="self-center mt-5 bg-teal-500 sm:mt-0"
+          onClick={onViewAll}
+        >
+          View All Announcements
+        </Button>
+      </div>
     </div>
   );
 };
