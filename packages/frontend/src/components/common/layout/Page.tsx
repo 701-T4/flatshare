18b8b9863e38d76bd6edec0607c199e55dfd899a
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-04-02 13:44:32 +1300

    Add BackButton component

--- packages/frontend/src/components/common/layout/Page.tsx
+++ packages/frontend/src/components/common/layout/Page.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Footer from './Footer';
 import Navigation from './Navigation';
 
-interface PageProps {}
+interface PageProps {
+  backpath?: string;
+}
 
 /**
  * Generic page used by pages in the app (i.e. not public pages)
  */
-const Page: React.FC<PageProps> = ({ children }) => {
+const Page: React.FC<PageProps> = ({ children, backpath }) => {
   return (
     <div className="flex flex-col w-full min-h-screen bg-gray-50">
       <div className="sticky top-0 z-50">
-        <Navigation />
+        <Navigation backpath={backpath} />
       </div>
 
       <div className="self-center flex-grow w-full max-w-5xl px-10">
