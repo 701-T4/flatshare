a0d8b8a0da922f9bb61fa19f64ff3968958c7049
Lance Delos Reyes <58032488+lanxdrey19@users.noreply.github.com>
2022-04-05 20:34:57 +1200

    Add comments to module files related to the Issue feature (#209)

--- packages/backend/src/controllers/issues/issues.module.ts
+++ packages/backend/src/controllers/issues/issues.module.ts
@@ -4,6 +4,12 @@ import { DbModule } from '../../db/db.module';
 import { FirebaseAuthStrategy } from '../../guards/firebase.auth';
 import { IssueUtil } from './issues.util';
 
+/**
+ * The issues module is responsible for registering which components
+ * are shared within this module through the providers and registering
+ * the controller that will be used for handling Issue-related requests.
+ * The imports include modules that this module depends on.
+ */
 @Module({
   controllers: [IssueController],
   imports: [DbModule],
